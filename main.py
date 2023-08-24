cf12f3154da71392ea6d387d3b297e61d211f2af
JungHanji <88892247+JungHanji@users.noreply.github.com>
2023-08-24 21:27:38 +0300

    post added

--- main.py
+++ main.py
@@ -7,14 +7,18 @@ app = Flask(__name__)
 def main():
     return 'Hello, World!'
 
+@app.route('/post')
+def postTest():
+	return request.form.get("data")
+
 def run():
-  app.run(host='0.0.0.0', port=900)
+	app.run(host='0.0.0.0', port=900)
 
 
 def keep_alive():
-  t = Thread(target=run)
-  t.start()
+	t = Thread(target=run)
+	t.start()
 
 
 if __name__ == '__main__':
-  keep_alive()
+	keep_alive()
