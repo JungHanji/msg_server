2519b1d9dc03d7d27b8e70ed1d003480fb7307e6
JungHanji <88892247+JungHanji@users.noreply.github.com>
2023-08-25 23:49:55 +0300

    7

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from MyDB import *
 
 app = Flask(__name__)
 
-CLIENTS_FILE = 'server_data/clients.mdb'
-MSGS_FILE = 'server_data/msgs.txt'
+CLIENTS_FILE = "server_data/clients.mdb"
+MSGS_FILE = "server_data/msgs.txt"
 
 if(not isExist(CLIENTS_FILE)): createFile(CLIENTS_FILE)
 if(not isExist(MSGS_FILE)): createFile(MSGS_FILE)
@@ -14,40 +14,40 @@ if(not isExist(MSGS_FILE)): createFile(MSGS_FILE)
 def main():
     return 'Nothing here'
 
-@app.route('/reg', methods=['POST'])
+@app.route('/reg', methods=["POST"])
 def regClient():
     jdict = getDictFromJSON(getlJSTRING(request))
     
-    name = jdict['name']
+    name = jdict["name"]
     if(not isLineInFile(CLIENTS_FILE, name)):
         addToFile(CLIENTS_FILE, name)
-        return 'Client added'
+        return "Client added"
     else:
-        return 'Client alredy exist'
+        return "Client alredy exist"
 
-@app.route('/newMsg', methods=['POST'])
+@app.route("/newMsg", methods=["POST"])
 def newMsg():
     jdict = getDictFromJSON(getlJSTRING(request))
     
-    name = jdict['name']
-    msg = jdict['msg']
+    name = jdict["name"]
+    msg = jdict["msg"]
     if(not isLineInFile(CLIENTS_FILE, name)):
-        return 'Clients isn`t exist'
+        return "Clients isn`t exist"
     else:
         addToFile(MSGS_FILE, f"{name} {msg}")
-        return f'[Messege posted] : {msg}'
+        return f"[Messege posted] : {msg}"
 
 
-@app.route('/getMsg', methods=['POST'])
+@app.route("/getMsg", methods=["POST"])
 def getMsg():
     jdict = getDictFromJSON(getlJSTRING(request))
-    name = jdict['name']
+    name = jdict["name"]
     if(len(readFileLines(MSGS_FILE))>0):
         return parseDLinesWhile(MSGS_FILE, name)
     else:
-        return 'No messeges to get'
+        return "No messeges to get"
 
-@app.route('/post', methods=["GET", "POST"])
+@app.route("/post", methods=["GET", "POST"])
 def postTest():
 	jsonstring = getDictFromJSON(getlJSTRING(request))
 	print("[POST] posted" + str(jsonstring))
